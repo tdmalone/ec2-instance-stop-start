e89c5dc0361625164382002be965b0ad03794e42
Tim Malone <tdmalone@gmail.com>
2018-03-14 13:20:23 +1100

    WIP: Bootstrapping function

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # EC2 Instance Stop Start
 
-Manages start and stop schedules to save costs on EC2 instances.
+A simple [AWS Lambda](https://aws.amazon.com/lambda/) function that helps manage start and stop schedules to save costs on EC2 instances.
+
+Based initially on [this AWS example](https://aws.amazon.com/premiumsupport/knowledge-center/start-stop-lambda-cloudwatch/).
+
+## License
+
+[MIT](LICENSE).
---
+++ lambda_function.py
@@ -0,0 +1,20 @@
+"""
+A simple AWS Lambda function that helps manage start and stop schedules to save costs on EC2
+instances.
+
+Initially based on this:
+https://aws.amazon.com/premiumsupport/knowledge-center/start-stop-lambda-cloudwatch/
+"""
+
+import boto3
+
+region = 'ap-southeast-2'
+instances = [ 'i-0155bb8e6d7dddf46' ]
+
+def lambda_handler( event, context ):
+
+  ec2 = boto3.client( 'ec2', region_name=region )
+  ec2.stop_instances( InstanceIds=instances )
+
+  print 'Stopped instances: ' + str( instances )
+  return { "event": "stop", "instances": instances }
